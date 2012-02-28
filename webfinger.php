7a6df19962753266abcfade2bdbc83833f46f253
Pelle Wessman <pelle@kodfabrik.se>
2012-02-28 10:37:24 +0100

    Added support for JRD

--- webfinger.php
+++ webfinger.php
@@ -13,13 +13,44 @@ if (!$acct) {
 	header("HTTP/1.1 404 Not Found");
 	die;
 }
-header('Content-type: application/xrd+xml');
-echo "<?xml version='1.0'?>\n"; ?>
-<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
+
+if (!empty($_GET['json'])) {
+	header('Content-type: application/json');
+	echo json_encode(array(
+		'subject' => 'acct:pelle@kodfabrik.se',
+		'aliases' => array('http://kodfabrik.se/'),
+		'links' => array(
+			array(
+				'rel' => 'http://webfinger.net/rel/profile-page',
+				'type' => 'text/html',
+				'href' => 'http://kodfabrik.se/',
+			),
+			array(
+				'rel' => 'http://microformats.org/profile/hcard',
+				'type' => 'text/html',
+				'href' => 'http://kodfabrik.se/',
+			),
+			array(
+				'rel' => 'http://gmpg.org/xfn/11',
+				'type' => 'text/html',
+				'href' => 'http://kodfabrik.se/',
+			),
+			array(
+				'rel' => 'describedby',
+				'type' => 'text/html',
+				'href' => 'http://kodfabrik.se/',
+			),
+		),
+	));
+} else {
+	header('Content-type: application/xrd+xml');
+	echo "<?xml version='1.0'?>\n";
+?><XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
 	<Subject>acct:pelle@kodfabrik.se</Subject>
 	<Alias>http://kodfabrik.se/</Alias>
 	<Link rel='http://webfinger.net/rel/profile-page' href='http://kodfabrik.se/' type='text/html'/>
 	<Link rel='http://microformats.org/profile/hcard' href='http://kodfabrik.se/' type='text/html'/>
 	<Link rel='http://gmpg.org/xfn/11' href='http://kodfabrik.se/' type='text/html'/>
 	<Link rel='describedby' href='http://kodfabrik.se/' type='text/html'/>
-</XRD>
+</XRD><?php
+}
